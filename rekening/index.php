33c206e379484dee354c3cf40920dfaa89870d64
JessenVallensio <130445208+JessenVallensio@users.noreply.github.com>
2023-12-09 18:32:34 +0700

    Add files via upload

---
+++ rekening/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Persembahan</title>
+  <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+<div class="container">
+    <nav>
+      <a href="/page1.html" class="active">Persepuluhan</a>
+      <a href="/page2.html">Pembangunan</a>
+    </nav>
+
+    <section class="page1">
+      <div class="card">
+        <div class="box box1">
+          <h1>BCA 0353 121 453</h1>
+        </div>
+        <p class="p">Transfer melalui Rekening BCA a/n GBI NEW LIFE COMMUNITY</p> <p style="color: white; text-align:justify;">Hai saudara-saudari yang dikasihi dalam Tuhan, Kita senantiasa bersyukur atas kasih dan berkat Tuhan yang melimpah kepada kita. Saat ini, kami ingin mengajak Anda untuk bersama-sama berpartisipasi dalam mendukung karya ibadah gereja melalui persembahan sukarela. Sebagai keluarga iman, kita memiliki tanggung jawab bersama untuk menjaga keberlanjutan dan keberlanjutan misi gereja. Persembahan yang kita berikan tidak hanya untuk memelihara tempat ibadah kita, tetapi juga untuk mendukung pelayanan dan proyek sosial yang melibatkan komunitas kita. Dengan sukacita dan niat tulus, mari kita berikan persembahan kita sebagai tanda syukur kita kepada Tuhan. Setiap donasi, sekecil apapun, memiliki dampak yang besar dalam membangun dan menguatkan gereja kita. Anda dapat memberikan persembahan melalui sarana yang telah disediakan di gereja atau melalui transfer bank dengan mencantumkan keterangan "Persembahan Persepuluhan" pada kolom berita. Terima kasih atas kepedulian dan dukungan Anda. Semoga Tuhan memberkati setiap tindakan baik yang kita lakukan untuk kemuliaan-Nya.</p>
+      </div>
+    </section>
+
+    <section class="page2">
+      <div class="card">
+        <div class="box box2" style="margin-bottom: 2rem;">
+          <h1>BCA 0353 121 496</h1>
+        </div>
+        <p class="pq">Transfer melalui Rekening BCA a/n GBI NEW LIFE COMMUNITY</p><p style="color: white; text-align:justify;">
+        Hai saudara-saudari yang terkasih dalam Tuhan, Dengan sukacita, kami ingin mengajak Anda untuk menjadi bagian dari proyek pembangunan gereja kita yang bertujuan untuk memperkuat dan memperluas tempat ibadah kita. Sebagai komunitas iman, kita memiliki visi bersama untuk menciptakan tempat yang nyaman dan ramah bagi setiap jiwa yang mencari Tuhan. Pembangunan gereja bukan hanya tentang bahan bangunan, tetapi juga tentang membangun fondasi iman kita. Setiap sumbangan yang Anda berikan akan membantu mewujudkan impian kita untuk memiliki ruang ibadah yang memadai, ruang pertemuan komunitas, serta fasilitas yang mendukung pelayanan dan kegiatan sosial. Mari kita bersama-sama membangun rumah Tuhan dengan hati sukarela dan sukacita. Setiap donasi, sekecil apapun, akan menjadi batu bata yang membangun masa depan gereja kita. Anda dapat menyumbangkan dana untuk pembangunan gereja melalui sarana yang telah disediakan di gereja atau dengan melakukan transfer bank dengan mencantumkan keterangan "Donasi Pembangunan Gereja" pada kolom berita. Terima kasih atas partisipasi dan dukungan Anda. Semoga Tuhan memberkati setiap langkah kita dalam merespon panggilan-Nya untuk membangun dan memuliakan nama-Nya.</p>
+      </div>      
+    </section>
+  </div>
+<!-- partial -->
+  <script  src="./script.js"></script>
+
+</body>
+</html>
