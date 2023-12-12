26d6d2987b28ec03598c33035e9de5082f3177ca
reeves63 <riffa.putra@student.umn.ac.id>
2023-12-12 08:43:08 +0700

    Update 2023_12_05_114556_media_table.php

--- App/database/migrations/2023_12_05_114556_media_table.php
+++ App/database/migrations/2023_12_05_114556_media_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('media', function (Blueprint $table) {
+        Schema::create('medias', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->string('link')->nullable();
