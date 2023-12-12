c81c4e7336777410b03efa163bd2f74658c1137b
JessenVallensio <130445208+JessenVallensio@users.noreply.github.com>
2023-12-12 12:30:44 +0700

    Add files via upload

---
+++ min/index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>ministries</title>
+  <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css'><link rel="stylesheet" href="./style.css">
+
+</head>
+<body>
+<body>
+    <section>
+      <div class="outer">
+        <div class="inner">
+          <div class="bg home">
+            <div class="scroll">
+              <p>Gereja Bethel Indonesia</p>
+              <p>Gereja Bethel Indonesia</p>
+            </div>
+            <div class="home-content">
+              <h1>
+                PELAYANAN
+              </h1>
+              <p>
+                Sebuah peluang diberikan kepada anggota jemaat GBI untuk berpartisipasi dalam pelayanan yang ada sesuai dengan bakat dan talenta yang dimilikinya.
+              </p>
+              <ul class="links">
+                <li>
+                  <a href="https://www.facebook.com/profile.php?id=100076716421749&mibextid=ZbWKwL"
+                    ><ion-icon class="icon" name="logo-facebook"></ion-icon></a>
+                </li>
+                <li>
+                  <a href="https://youtube.com/@gbinlcgadingserpong849?si=0XqOdbXhLMUltStu" target="_blank"><ion-icon class="icon" name="logo-youtube"></ion-icon
+                  ></a>
+                </li>
+                <li>
+                  <a href="https://www.instagram.com/gbinlc.gadingserpong/?utm_source=ig_web_button_share_sheet&igshid=OGQ5ZDc2ODk2ZA==" target="_blank"><ion-icon class="icon" name="logo-instagram"></ion-icon
+                  ></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section>
+      <div class="outer">
+        <div class="inner">
+          <div class="bg content">
+            <div class="swiper">
+              <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                  <img src="image/musik.jpg" alt="musik" />
+                  <div class="title">
+                    <h2>Pelayanan Musik</h2>
+                    <p>Permainan Alat Musik</p>
+                  </div>
+                </div>
+
+                <div class="swiper-slide">
+                  <img src="image/nyanyi.jpg" alt="nyanyi" />
+                  <div class="title">
+                    <h2>Pelayanan Menyanyi</h2>
+                    <p>Menyanyi Untuk Keperluan Ibadah</p>
+                  </div>
+                </div>
+
+                <div class="swiper-slide">
+                  <img src="image/minggu.jpg" alt="anak" />
+                  <div class="title">
+                    <h2>Pelayanan Guru</h2>
+                    <p>Menjadi Guru Sekolah Minggu Anak Anak</p>
+                  </div>
+                </div>
+
+                <div class="swiper-slide">
+                  <img src="image/media.jpg" alt="media" />
+                  <div class="title">
+                    <h2>Pelayanan Media</h2>
+                    <p>Melayani Monitoring Materi dan Perlengkapan</p>
+                  </div>
+                </div>
+              </div>
+              <div class="swiper-pagination"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section>
+      <div class="outer">
+        <div class="inner">
+          <div class="bg info">
+            <div class="info-content">
+              <h1>Kenapa harus melayani?</h1>
+              <p>
+                Melalui pelayanan terhadap jemaat GBI, kita dapat berbagi kasih, memperkuat ikatan komunitas, dan menjadi teladan bagi yang lain. Terlibat dalam pelayanan juga membawa pertumbuhan rohani pribadi, memenuhi panggilan rohani, dan memberikan kepuasan batin. Selain itu, pelayanan membuka peluang untuk mengembangkan bakat dan keterampilan, serta membantu memenuhi kebutuhan praktis gereja. Dengan merespons panggilan untuk melayani, kita tidak hanya mengenal lebih dekat sesama anggota gereja tetapi juga menciptakan atmosfer saling dukung yang memperkaya pengalaman hidup rohani bersama.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </body>
+<script src='https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js'></script>
+<script src='https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js'></script>
+<script src='https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js'></script>
+<script src='https://assets.codepen.io/16327/gsap-latest-beta.min.js'></script>
+<script src='https://assets.codepen.io/16327/Observer.min.js'></script>
+<script src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/16327/SplitText3.min.js'></script><script  src="./script.js"></script>
+
+</body>
+</html>
